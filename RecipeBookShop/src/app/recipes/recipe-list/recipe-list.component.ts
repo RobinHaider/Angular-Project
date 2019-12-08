2eedb242842795b0e558cdeae22e963e9a3170e9
Robin Haider <robinhaider69@gmail.com>
2019-12-08 16:11:13 +0600

    RecipeBookShop CM 4. Day three - routing complete

--- RecipeBookShop/src/app/recipes/recipe-list/recipe-list.component.ts
+++ RecipeBookShop/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
 import { RecipeService } from './../recipe.service';
 import { Recipe } from './../recipe.model';
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-list',
@@ -11,11 +12,16 @@ export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[];
 
-  constructor(private recipeService: RecipeService) { }
+  constructor(private recipeService: RecipeService,
+              private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
     this.recipes = this.recipeService.getRecipes();
   }
 
+  onNewRecipe(){
+    this.router.navigate(['new'], {relativeTo: this.route});
+  }
 
 }
