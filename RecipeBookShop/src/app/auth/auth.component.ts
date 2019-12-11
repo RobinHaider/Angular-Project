39aa3ce107bebaefcf144bea33ae3217b8e146c1
Robin Haider <robinhaider69@gmail.com>
2019-12-11 14:27:13 +0600

    RecipeBookShop CM 10. Day six - Authentication(login)

--- RecipeBookShop/src/app/auth/auth.component.ts
+++ RecipeBookShop/src/app/auth/auth.component.ts
@@ -1,6 +1,7 @@
-import { AuthService } from './auth.service';
+import { AuthService, AuthResponseData } from './auth.service';
 import { NgForm } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-auth',
@@ -10,6 +11,8 @@ import { Component, OnInit } from '@angular/core';
 export class AuthComponent implements OnInit {
 
   isLoginMode = true;
+  isLoading = false;
+  error: string = null;
 
   constructor(private authService: AuthService) { }
 
@@ -27,19 +30,27 @@ export class AuthComponent implements OnInit {
     const email = form.value.email;
     const password = form .value.password;
 
+    let authObs: Observable<AuthResponseData>;
+
+    this.isLoading = true;
+
     if(this.isLoginMode){
-      //...
+      authObs = this.authService.login(email,password);
     } else {
-      this.authService.signup(email,password)
-       .subscribe(
-         resData => {
-           console.log(resData);
-         },
-         error => {
-           console.log(error);
-         }
-       );
+      authObs = this.authService.signup(email,password);    
     }
+
+    authObs.subscribe(
+      resData => {
+        console.log(resData);
+        this.isLoading = false;
+      },
+      errorMsg => {
+        console.log(errorMsg);
+        this.error = errorMsg;
+        this.isLoading = false;
+      }
+    );
     
     form.reset();
   }
--- RecipeBookShop/src/app/auth/auth.service.ts
+++ RecipeBookShop/src/app/auth/auth.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 
-interface AuthResponseData {
+export interface AuthResponseData {
     idToken: string;
     email: string;
     refreshToken: string;
     expiresIn: string;
     localId: string;
+    registered?: string;
 }
 
 @Injectable({providedIn: 'root'})
@@ -17,6 +20,25 @@ export class AuthService {
 
     signup(email: string, password: string){
         return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCc5cEzIhoOPi5Z57FIO7kPtmD9PIEy1Os', {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        })
+        .pipe(catchError(errorRes => {
+            let errorMsg = 'An unknown error occurred';
+            if(!errorRes.error || !errorRes.error.error){
+                return throwError(errorMsg);
+            }
+            switch(errorRes.error.error.message){
+                case 'EMAIL_EXISTS':
+                    errorMsg = "This email already exists.";
+            }
+            return throwError(errorMsg);
+        }));
+    }
+
+    login(email: string, password: string){
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCc5cEzIhoOPi5Z57FIO7kPtmD9PIEy1Os',{
             email: email,
             password: password,
             returnSecureToken: true
