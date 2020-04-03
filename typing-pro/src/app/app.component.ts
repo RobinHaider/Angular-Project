2665ca6c93c9bd660cc5331c43ca80c3a388a9f4
Robin Haider <robinhaider69@gmail.com>
2020-04-03 19:58:13 +0600

    The Modern Angular Bootcamp 2020 - Day 3 - 4 - Typing Pro - some improvement

--- typing-pro/src/app/app.component.ts
+++ typing-pro/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import {lorem} from 'faker';
 
 @Component({
@@ -7,18 +7,49 @@ import {lorem} from 'faker';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
   inputText = '';
   randomText = lorem.sentence();
+  correct = 0;
+  incorrect = 0;
+  acuricy = 0;
+  overallAcuricy = 0;
+  finishMsg = false;
 
   onChangeInput(value: string){
     this.inputText = value;
-    // let randomTextArr = this.randomText.split('');
-    // let inputTextArr = inputText.split('');
-
-    // for(let i=0; i<inputTextArr.length; i++){
-
-    // }
   }
+
+  onClickNext(){
+    this.inputText = '';
+    this.randomText = lorem.sentence();
+    this.correct = 0;
+    this.incorrect = 0;
+    this.acuricy = 0;
+    this.finishMsg = false;
+  }
+
+  onClickDone(){
+    const randomTextArr = this.randomText.split('');
+    const inputTextArr = this.inputText.split('');
+
+    for(let i=0; i<inputTextArr.length; i++){
+
+      if(inputTextArr[i] === randomTextArr[i]) {
+        this.correct++;
+      } else {
+        this.incorrect++;
+      }
+    }
+
+    //acuricy calculation
+    this.acuricy = Math.floor((this.correct/randomTextArr.length) * 100);
+    this.overallAcuricy = this.overallAcuricy === 0? Math.floor(this.acuricy) :
+     Math.floor((this.overallAcuricy + this.acuricy) / 2);
+
+    this.finishMsg = true;
+  }
+
 
   correctColor(randomLetter: string, inputLetter: string){
     if(!inputLetter){
