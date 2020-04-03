ae7b53c820199dc5fb1b74e45126e2a93eb6fb8a
Robin Haider <robinhaider69@gmail.com>
2020-04-04 00:38:39 +0600

    The Modern Angular Bootcamp 2020 - Day 3 - 6 - typing pro improved

--- typing-pro/src/app/app.component.ts
+++ typing-pro/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import {lorem} from 'faker';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,29 +8,41 @@ import {lorem} from 'faker';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  @ViewChild('input_value', {static: false}) textBox: ElementRef;
+
   inputText = '';
   randomText = lorem.sentence();
   correct = 0;
   incorrect = 0;
   acuricy = 0;
   overallAcuricy = 0;
+
   finishMsg = false;
+  doneClicked = false;
 
   onChangeInput(value: string){
     this.inputText = value;
   }
 
   onClickNext(){
+    this.textBox.nativeElement.value = '';
     this.inputText = '';
     this.randomText = lorem.sentence();
     this.correct = 0;
     this.incorrect = 0;
     this.acuricy = 0;
     this.finishMsg = false;
+    this.doneClicked = false;
   }
 
   onClickDone(){
+    if(this.doneClicked){
+      return this.onClickNext();
+    }
+    if(!this.inputText){
+      return;
+    }
+
     const randomTextArr = this.randomText.split('');
     const inputTextArr = this.inputText.split('');
 
@@ -48,6 +61,10 @@ export class AppComponent {
      Math.floor((this.overallAcuricy + this.acuricy) / 2);
 
     this.finishMsg = true;
+    this.textBox.nativeElement.value = '';
+    this.inputText='';
+    this.doneClicked = true;
+  
   }
 
 
